64246446cf65d11b4a784d41d93a48dbfc28748d
ripterdust <bryantello2010@hotmail.com>
2022-05-23 19:32:05 -0600

    Added the main logic

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,7 +6,42 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    // Login
     public function create(){
         return view('auth.login');
     }
+
+    // Store user
+    public function store(Request $request){
+        $request->validate([
+            "email" => "required",
+            "password" => "required"
+        ]);
+
+        if(auth()->attempt(request(['email', 'password'])) == false){
+            return back()->withError([
+                'message' => 'El correo o la contraseña son incorrectas'
+            ]);
+        }
+
+        return redirect()->route('home');
+    }
+
+    // Logout
+
+    public function destroy(){
+        auth()->logout();
+        return redirect()->route('home');
+    }
+
+    // Account view
+    public function show(){
+        $response = [
+            'user' => auth()->user()
+        ];
+
+        return view('auth.account', $response);
+    }
+
+   
 }
