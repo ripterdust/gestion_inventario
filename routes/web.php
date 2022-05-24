76261a751bde4ffc3977e3845432f528cd26c301
ripterdust <bryantello2010@hotmail.com>
2022-05-23 19:15:15 -0600

    Added the controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+// Authentication
+
+// Login
+Route::get('/login', [SessionsController::class, 'index'])
+    ->middleware('auth')
+    ->name('login');
