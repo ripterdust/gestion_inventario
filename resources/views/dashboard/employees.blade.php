f5b080b7c063c738a76cab883e1bfb93848f25b3
ripterdust <bryantello2010@hotmail.com>
2022-05-25 14:00:37 -0600

    Added the general styles

--- resources/views/dashboard/employees.blade.php
+++ resources/views/dashboard/employees.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Employees')
 
 @section('content')
-    <div class="table_list employees">
+    <div class="table_list employees box">
         <div class="title">Empleados</div>
 
         <table class="table">
--- resources/views/dashboard/tests.blade.php
+++ resources/views/dashboard/tests.blade.php
@@ -16,4 +16,33 @@
             <div class="icon"><i class="fa-solid fa-vial"></i></div>
         </div>
     </div>
+
+    <div class="table_list box">
+        <div class="title">Exámenes</div>
+
+        <table class="table">
+            <!-- Table title -->
+            <tr>
+                <th>Cliente</th>
+                <th>Examen</th>
+                <th>Ingreso</th>
+                <th>Estado</th>
+                <th></th>
+            </tr>
+
+            <!-- Table body -->
+            <tr>
+                <td>Juanito perex</td>
+                <td>Hemoglobina</td>
+                <td>12/02/22</td>
+                <td>Proceso</td>
+                <td>
+                    <a class="action">
+                        <i class="fa-solid fa-eye"></i>
+                    </a>
+                </td>
+            </tr>
+
+        </table>
+    </div>
 @endsection
