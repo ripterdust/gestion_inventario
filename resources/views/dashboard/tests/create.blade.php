5780377592fdeaf4fc1fb7b747ec1261a9f23365
ripterdust <bryantello2010@hotmail.com>
2022-05-25 21:29:05 -0600

    Added form::new_Test

--- resources/views/dashboard/tests/create.blade.php
+++ resources/views/dashboard/tests/create.blade.php
@@ -7,5 +7,17 @@
         <div class="title">
             Nuevo examen
         </div>
+
+        <div class="form">
+            <div class="form-control">
+                <label for="Client">Nompre cliente:</label>
+                <input type="text" name="client" id="clientAutocomplete" />
+            </div>
+
+            <div class="form-control">
+                <label for="">Examen:</label>
+                <input type="text" name="testName" id="testAutocomplete">
+            </div>
+        </div>
     </div>
 @endsection
