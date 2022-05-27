376e1edd0a663043577496a76d2447b94d10f168
ripterdust <bryantello2010@hotmail.com>
2022-05-27 11:51:40 -0600

    Finished the first part of the form test

--- resources/views/dashboard/tests/edit.blade.php
+++ resources/views/dashboard/tests/edit.blade.php
@@ -5,29 +5,26 @@
 @section('content')
 
 <div class="box">
-    <div class="title">{{ $name }}</div>
+    <div class="title">{!! $name !!}</div>
 
-    <form method="post" action="" class="test-form" id="form-test">
+    <form action="" class="formDataTest">
         @method('post')
         @csrf
-        <div class="params" id="params">
-        </div>
-        <input type="submit" value="Validar datos" class="button" />
+        <div class="params" id="params"></div>
+        <input type="submit" value="Guardar" class="button" />
     </form>
 </div>
-
 
 <script>
     const parameters = JSON.parse({!! $parameters !!}[0]?.parameters);
     const form = document.querySelector('#params');
-
     Object.keys(parameters).map((paramFind) => {
         const param = parameters[paramFind];
         form.innerHTML += `
-            <div class="test-container">
+            <div class="param">
                 <span class="name">${paramFind}</span>
-                <input type="text" class="value" name="${paramFind}" />
-                <span class="params">${param}</span>
+                <input type="text" class="value" name="${paramFind}" placeholder="Ingresa el valor" />
+                <span class="paramsTest">${param}</span>
             </div>
         `;
     })
