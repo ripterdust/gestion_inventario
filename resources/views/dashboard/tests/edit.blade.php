889ced32ea96edc4a9d45cafb09d040e8e5db39c
ripterdust <bryantello2010@hotmail.com>
2022-05-27 11:33:09 -0600

    Added the main form logic

--- resources/views/dashboard/tests/edit.blade.php
+++ resources/views/dashboard/tests/edit.blade.php
@@ -7,11 +7,30 @@
 <div class="box">
     <div class="title">{{ $name }}</div>
 
+    <form method="post" action="" class="test-form" id="form-test">
+        @method('post')
+        @csrf
+        <div class="params" id="params">
+        </div>
+        <input type="submit" value="Validar datos" class="button" />
+    </form>
 </div>
 
 
 <script>
     const parameters = JSON.parse({!! $parameters !!}[0]?.parameters);
-    console.log(parameters)
+    const form = document.querySelector('#params');
+
+    Object.keys(parameters).map((paramFind) => {
+        const param = parameters[paramFind];
+        form.innerHTML += `
+            <div class="test-container">
+                <span class="name">${paramFind}</span>
+                <input type="text" class="value" name="${paramFind}" />
+                <span class="params">${param}</span>
+            </div>
+        `;
+    })
+
 </script>
 @endsection
