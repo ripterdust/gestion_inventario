96080626aca89dba9b81dba5b37ffcc1a0d8d303
ripterdust <bryantello2010@hotmail.com>
2022-05-26 20:30:29 -0600

    Added the box

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -3,5 +3,21 @@
 @section('title', 'Inventario')
 
 @section('content')
-    Inventario
+    <div class="metrics">
+        <div class="metric">
+            <div class="text">
+                <div class="title">Total productos</div>
+                <div class="number">
+                    100
+                </div>
+            </div>
+            <div class="icon">
+                <i class="fa-solid fa-boxes-stacked"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="title">Inventario</div>
+    </div>
 @endsection
