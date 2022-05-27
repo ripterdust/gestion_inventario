828bfd2f3ff38bd28e6e74cd5fa73dcdbf393429
ripterdust <bryantello2010@hotmail.com>
2022-05-26 20:04:14 -0600

    Added the sales view

---
+++ resources/views/sales/sales.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Ventas')
+
+<div class="metrics">
+    <div class="metric">
+        <div class="text">
+            <div class="title">Total ventas</div>
+            <div class="number">1000</div>
+        </div>
+        <div class="icon">
+            <i class="fa-solid fa-money-bill-trend-up"></i>
+        </div>
+    </div>
+    <div class="metric">
+        <div class="text">
+            <div class="title">Ventas del mes</div>
+            <div class="number">1000</div>
+        </div>
+        <div class="icon">
+            <i class="fa-solid fa-coins"></i>
+        </div>
+    </div>
+</div>
+
+@endsection
