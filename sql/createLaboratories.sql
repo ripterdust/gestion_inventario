ec5e8de59487afa648f814e3149362f45ca35580
ripterdust <bryantello2010@hotmail.com>
2022-05-26 20:51:06 -0600

    Added the example lab update

--- sql/createLaboratories.sql
+++ sql/createLaboratories.sql
@@ -21,32 +21,38 @@ values('HEMATOLOGIA'),
 /* Exámenes de hematología */
 
 /* ('', '', '[]', '[]') */
-INSERT INTO laboratories(category, name, parameters, ranges)
+INSERT INTO laboratories(category, name, parameters)
 values 
-('HEMATOLOGIA', 'HEMATOLOGIA COMPLETA 22/P ERITROSEDIMENTACION INCLUIDA', '[]', '[]'),
-('HEMATOLOGIA', 'ERITROSEDIMENTACION', '[]', '[]'),
-('HEMATOLOGIA', 'CLASIFICACION DE ANEMIAS', '[]', '[]'),
-('HEMATOLOGIA', 'FROTE PERIFERICO', '[]', '[]'),
-('HEMATOLOGIA', 'GOTA GRUESA', '[]', '[]'),
-('HEMATOLOGIA', 'GRUPO SANGUINEO Y FACTOR RH', '[]', '[]'),
-('HEMATOLOGIA', 'HEMOGLOBINA- HEMATOCRITO', '[]', '[]'),
-('HEMATOLOGIA', 'RECUENTO DE BLANCOS Y FORMULA', '[]', '[]'),
-('HEMATOLOGIA', 'RECUENTO DE EOSINOFILOS', '[]', '[]'),
-('HEMATOLOGIA', 'RECUENTO DE ERITROCITOS', '[]', '[]'),
-('HEMATOLOGIA', 'RECUENTO DE PLAQUETAS ', '[]', '[]'),
-('HEMATOLOGIA', 'Recuento de Reticulositos', '[]', '[]'),
-('HEMATOLOGIA', 'CRIOGLOBULINAS', '[]', '[]'),
-('HEMATOLOGIA', 'ELECTROFORESIS DE HEMOGLOBINA', '[]', '[]'),
-('HEMATOLOGIA', 'ELECTROFORESIS DE PROTEINAS', '[]', '[]'),
-('HEMATOLOGIA', 'ELECTROFORESIS DE LIPOPROTEINAS', '[]', '[]');
+('HEMATOLOGIA', 'HEMATOLOGIA COMPLETA 22/P ERITROSEDIMENTACION INCLUIDA', '{}'),
+('HEMATOLOGIA', 'ERITROSEDIMENTACION', '{}'),
+('HEMATOLOGIA', 'CLASIFICACION DE ANEMIAS', '{}'),
+('HEMATOLOGIA', 'FROTE PERIFERICO', '{}'),
+('HEMATOLOGIA', 'GOTA GRUESA', '{}'),
+('HEMATOLOGIA', 'GRUPO SANGUINEO Y FACTOR RH', '{}'),
+('HEMATOLOGIA', 'HEMOGLOBINA- HEMATOCRITO', '{}'),
+('HEMATOLOGIA', 'RECUENTO DE BLANCOS Y FORMULA', '{}'),
+('HEMATOLOGIA', 'RECUENTO DE EOSINOFILOS', '{}'),
+('HEMATOLOGIA', 'RECUENTO DE ERITROCITOS', '{}'),
+('HEMATOLOGIA', 'RECUENTO DE PLAQUETAS ', '{}'),
+('HEMATOLOGIA', 'Recuento de Reticulositos', '{}'),
+('HEMATOLOGIA', 'CRIOGLOBULINAS', '{}'),
+('HEMATOLOGIA', 'ELECTROFORESIS DE HEMOGLOBINA', '{}'),
+('HEMATOLOGIA', 'ELECTROFORESIS DE PROTEINAS', '{}'),
+('HEMATOLOGIA', 'ELECTROFORESIS DE LIPOPROTEINAS', '{}');
 
 
 /* HEMOTASIS */
-INSERT INTO laboratories(category, name, parameters, ranges)
+INSERT INTO laboratories(category, name, parameters)
 values 
-('HEMOTASIS', 'TIEMPO DE PROTOMBINA (TP)', '[]', '[]'),
-('HEMOTASIS', 'TIEMPOPARCIAL DE TROMBOPLASTINA (PTP)', '[]', '[]'),
-('HEMOTASIS', 'INR', '[]', '[]'),
-('HEMOTASIS', 'TIEMPO DE COAGULACION', '[]', '[]'),
-('HEMOTASIS', 'TIEMPO DE SANGRIA', '[]', '[]'),
-('HEMOTASIS', 'FIBRIOGENO', '[]', '[]');
+('HEMOTASIS', 'TIEMPO DE PROTOMBINA (TP)', '{}'),
+('HEMOTASIS', 'TIEMPOPARCIAL DE TROMBOPLASTINA (PTP)', '{}'),
+('HEMOTASIS', 'INR', '{}'),
+('HEMOTASIS', 'TIEMPO DE COAGULACION', '{}'),
+('HEMOTASIS', 'TIEMPO DE SANGRIA', '{}'),
+('HEMOTASIS', 'FIBRIOGENO', '{}');
+
+
+update laboratories 
+set 
+    parameters = '{"Rec. Globulos blancos":"5000 - 10,000 mm3","Rec. Globulos rojos":"4,5000,000 - 10,000,00 mm3","Hemoglobina":"12.6 - 16.0 gr/dr","Plaquetas":"150.000 - 500.00/ mm3","Formula Referencia":"","Segmentados":"45 - 64%","Linfositos":"20 - 45%","Monocitos":"0 - 5%","Eosinofilos":"1.0 - 5.0%","Velocidad de sedmentación":"0 - 15 mm/hrs"}'
+where id = 1;
