dd9d3738a75b905cf087414972289c6ddafdfa4e
ripterdust <bryantello2010@hotmail.com>
2022-06-12 13:37:28 -0600

    Added the main form styles

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -8,7 +8,22 @@
     <div class="box">
         <div class="title">Nuevo paciente</div>
 
+        {{-- Form to add the client --}}
+
+        <form action="/" method="post" class="form">
+        @csrf
+        @method('POST')
         
+        <div class="form-control">
+            <input 
+            type="text"
+            class="form-control-input"
+            autocomplete="off"
+            placeholder=" " 
+            required/>
+            <label for="name" class="form-control-label">Nombre paciente</label>    
+        </div>
+        </form>
 
     </div>
 
