943a04c220311842326ff02cc7360f0dfb7bcafa
ripterdust <bryantello2010@hotmail.com>
2022-06-12 13:40:57 -0600

    Added some form inputs

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -14,15 +14,49 @@
         @csrf
         @method('POST')
         
-        <div class="form-control">
-            <input 
-            type="text"
-            class="form-control-input"
-            autocomplete="off"
-            placeholder=" " 
-            required/>
-            <label for="name" class="form-control-label">Nombre paciente</label>    
-        </div>
+            <div class="form-control">
+                <input 
+                type="text"
+                class="form-control-input"
+                autocomplete="off"
+                placeholder=" " 
+                name="name"
+                required/>
+                <label for="name" class="form-control-label">Nombre completo</label>    
+            </div>
+
+            <div class="form-control">
+                <input 
+                type="email"
+                class="form-control-input"
+                autocomplete="off"
+                placeholder=" "
+                name="email"
+                required/>
+                <label for="email" class="form-control-label">Correo electrónico</label>    
+            </div>
+
+            <div class="form-control">
+                <input 
+                type="text"
+                class="form-control-input"
+                autocomplete="off"
+                placeholder=" "
+                name="adress"
+                required/>
+                <label for="adress" class="form-control-label">Dirección</label>
+            </div>
+                    
+            <div class="form-control">
+                <input 
+                type="text"
+                class="form-control-input"
+                autocomplete="off"
+                placeholder=" "
+                name="phone"
+                required/>
+                <label for="phone" class="form-control-label">Teléfono</label>
+            </div>
         </form>
 
     </div>
