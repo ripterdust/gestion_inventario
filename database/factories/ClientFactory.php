d8af6abe44779e90855098b9ee836e6711b98c53
ripterdust <bryantello2010@hotmail.com>
2022-06-12 20:57:15 -0600

    Added the client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use Faker\Generator as Faker;
+
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
  */
@@ -14,10 +17,15 @@ class ClientFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(Faker $faker)
     {
         return [
-            //
+            'name' => $faker->name,
+            'mail' => $faker->unique()->safeEmail,
+            'nit' => '1413235k',
+            'birth' => '12-02-2000',
+            'phone' => '124134134',
+            'adress' => '51 avenida a 6-23 residencial naciones unidas 2, zona 10 de Villa Nueva'
         ];
     }
 }
