4a69244f112e6843eee4dfcfcbc821252d4d6296
cvasquez-energetika <cvasquez@energetika.cl>
2022-07-17 15:20:21 -0600

    scripts de las nuevas tablas a colocar

--- sql/laboratories/lab_fields.sql
+++ sql/laboratories/lab_fields.sql
@@ -1,7 +1,8 @@
-create table lab_fields(
-    id      int auto_increment,
-    field   varchar(255),
-    lab_id  int not null,
-    PRIMARY KEY     (id),
-    FOREIGN KEY     (lab_id) REFERENCES lab_list(id)
+CREATE TABLE fields(
+    lab_id          INT NOT NULL AUTO_INCREMENT,
+    fields_pm       VARCHAR(50) NOT NULL,
+    fields_rgmin    INT NOT NULL,
+    fields_rgmax    INT NOT NULL,
+    fields_tp       CHAR(5),
+    FOREIGN KEY (lab_id) REFERENCES laboratory(lab_cd)
 )
