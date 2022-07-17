9ef65a8deaf34eb2d6a32eaaafb5787027aeab60
cvasquez-energetika <cvasquez@energetika.cl>
2022-07-17 12:28:06 -0600

    script de tabla completado

--- sql/laboratories/lab_fields.sql
+++ sql/laboratories/lab_fields.sql
@@ -1,5 +1,7 @@
 create table lab_fields(
     id      int auto_increment,
-    field   varchar(255)
-    lab_id  int not null
+    field   varchar(255),
+    lab_id  int not null,
+    PRIMARY KEY     (id),
+    FOREIGN KEY     (lab_id) REFERENCES lab_list(id)
 )
