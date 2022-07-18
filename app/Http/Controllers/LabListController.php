586e7b0d3625a41d9dad3610314e531c0da565e4
cvasquez-energetika <cvasquez@energetika.cl>
2022-07-18 11:27:18 -0600

    controlador lab_lists paginacion modificada

--- app/Http/Controllers/LabListController.php
+++ app/Http/Controllers/LabListController.php
@@ -3,8 +3,94 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Lab_list;
 
 class LabListController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $count = Lab_list::all()->count();
+
+        $laboratories = DB::table('lab_lists')
+        ->join('fields', 'lab_lists.field_id', '=', 'fields.field_id')
+        ->join('categories', 'lab_lists.cat_id', '=', 'categories.id')
+        ->select('lab_lists.lab_name', 'categories.category', 'lab_lists.lab_pc')
+        ->paginate(4);
+
+        return view('laboratory.index', [
+            'count' => $count,
+            'laboratories' => $laboratories
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
