219addcb074d063d4d3cd9f8174feb1f1b72ff36
Bryan Arévalo <barevalo@solcomp.com>
2022-08-02 22:06:35 -0600

    :rocket: Agregada la guía de la queries y el response hanlder

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,9 +10,12 @@ class Client extends Model
     use HasFactory;
 
     // fetch nit
+    // @param $nit string
+    // @return { err: boolean, message? : string, data: Client}
     public function getClientByNit($nit){
         // Paso 1 -> Buscar en la base de datos y si no se encuentra, dejar preparado para buscar en la web
         // Paso 2 -> retornar el cliente
+
     }
 
 }
